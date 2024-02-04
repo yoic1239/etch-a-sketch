1ea4366d55206bfb7b816b4d26b1aaabb22257e0
Oi Ching Yip <67258773+yoic1239@users.noreply.github.com>
2024-02-03 23:40:40 -0700

    Add 16x16 grid of square divs

--- javascript.js
+++ javascript.js
@@ -1,2 +1,13 @@
 const container = document.querySelector('.container');
 
+for (let i = 0; i < 16; i++) {
+    const row = document.createElement('div');
+    row.classList.add('row'); 
+    container.appendChild(row);
+
+    for (let j = 0; j < 16; j++) {
+        const grid = document.createElement('div');
+        grid.classList.add('grid');
+        row.appendChild(grid);
+    }
+}
