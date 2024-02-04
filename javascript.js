d76f5fb2e74d1fcfc948f501e3e5f4d911693519
Oi Ching Yip <67258773+yoic1239@users.noreply.github.com>
2024-02-04 00:04:57 -0700

    Add button to update squares per size

--- javascript.js
+++ javascript.js
@@ -1,21 +1,40 @@
-const container = document.querySelector('.container');
-
-for (let i = 0; i < 16; i++) {
-    const row = document.createElement('div');
-    row.classList.add('row'); 
-    container.appendChild(row);
-
-    for (let j = 0; j < 16; j++) {
-        const grid = document.createElement('div');
-        grid.classList.add('grid');
-        row.appendChild(grid);
-
-        grid.addEventListener('mouseover', function() {
-            grid.style.backgroundColor = randomColor();
-        });
-    }
-}
-
 function randomColor() {
     return `rgb(${Math.floor(Math.random()*266)}, ${Math.floor(Math.random()*266)}, ${Math.floor(Math.random()*266)})`;
 }
+
+function createGrids(squarePerSide) {
+    for (let i = 0; i < squarePerSide; i++) {
+        const row = document.createElement('div');
+        row.classList.add('row'); 
+        container.appendChild(row);
+    
+        for (let j = 0; j < squarePerSide; j++) {
+            const grid = document.createElement('div');
+            grid.classList.add('grid');
+            row.appendChild(grid);
+    
+            grid.addEventListener('mouseover', function() {
+                grid.style.backgroundColor = randomColor();
+            });
+        }
+    }
+}
+
+function clearGrids() {
+    container.innerHTML = '';
+}
+
+const container = document.querySelector('.container');
+const updateBtn = document.querySelector('.update-size');
+
+createGrids(16);
+
+updateBtn.addEventListener('click', function() {
+    let squarePerSide = 0;
+    do {
+        squarePerSide = Number(prompt('Please enter the number of squares per side (Max: 100)'));
+    } while (!Number.isInteger(squarePerSide) || squarePerSide < 1 || squarePerSide > 100);
+    
+    clearGrids();
+    createGrids(squarePerSide);
+});
